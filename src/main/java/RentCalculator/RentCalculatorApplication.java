fa8e641325f12468afa64ad6c4caeabdf77fdd7a
jdint <i.kuzmin@do-it.co>
2021-09-06 18:51:43 +0300

    init commit

---
+++ src/main/java/RentCalculator/RentCalculatorApplication.java
@@ -0,0 +1,11 @@
+package RentCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class RentCalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RentCalculatorApplication.class, args);
+    }
+}
