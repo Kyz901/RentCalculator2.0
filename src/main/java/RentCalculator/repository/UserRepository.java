3b265ef95b49fa14ac899eda52218f62c7b6bb63
Ihor Kyzmin <kyz9.01.01@gmail.com>
2021-09-25 14:58:01 +0300

    [DP-I14] - fixed delete query

--- src/main/java/RentCalculator/repository/UserRepository.java
+++ src/main/java/RentCalculator/repository/UserRepository.java
@@ -2,9 +2,11 @@ package RentCalculator.repository;
 
 import RentCalculator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,15 +21,16 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     @Query(value = "SELECT u.id, u.first_name, u.second_name, u.email, u.login, u.password, u.is_deleted " +
                    "FROM rentcalculator.users u " +
-                   "WHERE u.is_deleted = FALSE" +
+                   "WHERE u.is_deleted = FALSE " +
                    "AND u.id = :userId",
                     nativeQuery = true)
     User getUserById(@Param("userId") Integer userId);
 
-    @Query(value = "UPDATE rentcalculator.users u" +
-                   "SET u.is_deleted = TRUE" +
-                   "WHERE u.id = :userId",
-                    nativeQuery = true)
-    void deleteUser(@Param("userId") Integer userId);
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE User u " +
+                   "SET u.isDeleted = TRUE " +
+                   "WHERE u.id = :userId")
+    Integer deleteUser(@Param("userId") Integer userId);
 
 }
